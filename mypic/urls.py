b11936aea4c1e54068de3f405a06e5dd8e93207a
Teri11 <augustineteri157@gmail.com>
2022-03-27 21:35:49 +0300

    added search path

--- mypic/urls.py
+++ mypic/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path,register_converter
 from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.home,name='homepage')
+    path('',views.home,name='homepage'),
+    path('search/', views.search_results, name='search_results'),
 ]
 
 if settings.DEBUG:
