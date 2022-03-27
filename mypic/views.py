9a0f40248892740ba51852faab775ea4e9dd3dd6
Teri11 <augustineteri157@gmail.com>
2022-03-27 09:17:27 +0300

    added link for render db

--- mypic/views.py
+++ mypic/views.py
@@ -1,6 +1,9 @@
 from django.http  import HttpResponse,Http404
 from django.shortcuts import render
+from .models import Category,Picture
+
 
 # Create your views here.
 def home(request):
-  return render(request, 'index.html')
+    picture=Picture.objects.all()   
+    return render(request, 'index.html',{'picture':picture[::-1],})
