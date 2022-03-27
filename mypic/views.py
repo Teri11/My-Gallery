9b010d03a8364cdd468a526a27981cf8a80f8d90
Teri11 <augustineteri157@gmail.com>
2022-03-27 21:35:18 +0300

    added search category

--- mypic/views.py
+++ mypic/views.py
@@ -7,3 +7,20 @@ from .models import Category,Picture
 def home(request):
     picture=Picture.objects.all()   
     return render(request, 'index.html',{'picture':picture[::-1],})
+
+
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        searched_articles = Picture.search_by_category( category)
+        message = f"{ category}"
+
+        return render(request, 'search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+    
+    
